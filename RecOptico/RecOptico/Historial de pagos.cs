499176dfcd265cef8569286510580bd977ab2423
Jesus Arellano <jesushrz00@gmail.com>
2021-06-15 19:38:06 -0700

    Agregué forma historial de pagos

---
+++ RecOptico/RecOptico/Historial de pagos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecOptico
+{
+    public partial class Historial_de_pagos : Form
+    {
+        public Historial_de_pagos()
+        {
+            InitializeComponent();
+        }
+    }
+}
